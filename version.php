d7a232a7e85abedb1fccdf117dc29b5f3de80594
prowebat <1819129+prowebat@users.noreply.github.com>
2026-03-03 20:05:55 +0800

    require moodle 4.5

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exaaichat';
-$plugin->version = 2026030301;
-$plugin->requires = 2022041600;
+$plugin->version = 2026030302;
+$plugin->requires = 2024100700; // moodle 4.5
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '5.1';
